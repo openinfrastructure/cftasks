682e9ade6193d738b0a18344501b8d51cbb3b17c
Jeff McCune <jeff@openinfrastructure.co>
2019-07-18 11:18:44 -0700

    Add initial structure
    References:
    
     * https://packaging.python.org/tutorials/packaging-projects/
     * https://github.com/pyinvoke/invoke/blob/master/setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cftasks",
+    version="0.0.1",
+    author="Open Infrastructure Services, LLC",
+    author_email="code@openinfrastructure.co",
+    description="Shared tasks for local development and CI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/openinfrastructure/cftasks",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
